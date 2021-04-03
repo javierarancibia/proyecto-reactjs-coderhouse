528dda7cb81b8cbef7fd733d35f3eeac8acb51c9
Javier <javier.arancibia.reyes@hotmail.com>
2021-04-03 19:48:25 -0300

    Primera entrega Proyecto React JS Cocerhouse - Javier Arancibia

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Imagen de pantalla Desafio 1 - React JS Coderhouse</p>
         <a
           className="App-link"
           href="https://reactjs.org"
